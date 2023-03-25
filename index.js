4854335ab69a8951ac4871ea289564b1ca05780c
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-03-25 14:18:16 -0700

    moved html generation to javascript instead of index.html

--- index.js
+++ index.js
@@ -6,13 +6,26 @@ const fetchData = async (searchTerm) => {
     },
   });
 
-if (response.data.Error){
-  return [];
-}  
+  if (response.data.Error) {
+    return [];
+  }
   return response.data.Search;
 };
 
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<label><b>Search for a Movie</b></label>
+<input class="input" />
+<div class="dropdown is-active">
+  <div class="dropdown-menu">
+    <div class="dropdown-content results"></div>
+  </div>
+</div>
+`;
+
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
