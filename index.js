6607ba72cbf946c28898e5e944aded3f1967657d
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-03-25 13:11:09 -0700

    debounce function
    refactored timeout on search and added it to a debounce function

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// Here is your key: 315ed2b
-// Please append it to all of your API requests,
-// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=315ed2b
-
 const fetchData = async (searchTerm) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
@@ -13,14 +9,20 @@ const fetchData = async (searchTerm) => {
 };
 
 const input = document.querySelector("input");
-
-let timeoutId;
-const onInput = event => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  timeoutId = setTimeout(() => {
-    fetchData(event.target.value);
-  }, 500);
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutIdsetTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
 };
-input.addEventListener("input", onInput);
+
+const onInput = (event) => {
+  fetchData(event.target.value);
+});
+
+input.addEventListener("input", debounce(onInput, 500));
